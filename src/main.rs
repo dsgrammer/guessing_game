91754cc7c5fd1ac9f9fc1181653478625f1b6ea6
dsgrammer <dsgrammer85@gmail.com>
2023-05-19 21:32:13 +0900

    fmt update

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Guess the number!");
@@ -13,7 +13,7 @@ fn main() {
         println!("Please input your guess.");
 
         let mut guess = String::new();
-    
+
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
@@ -22,9 +22,9 @@ fn main() {
             Ok(num) => num,
             Err(_) => continue,
         };
-    
+
         println!("You guessed: {guess}");
-    
+
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
@@ -34,5 +34,4 @@ fn main() {
             }
         }
     }
-
 }
